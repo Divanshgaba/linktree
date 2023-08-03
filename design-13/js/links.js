f254b0582644350056e7dcf9ff11b51e888df35b
Divanshgaba <110758780+Divanshgaba@users.noreply.github.com>
2023-08-03 16:03:46 +0530

    new theme 13

---
+++ design-13/js/links.js
@@ -0,0 +1,37 @@
+export const links = [
+    {
+      name: "Twitter",
+      link: "https://twitter.com/",
+      image: "./assets/twitter.png",
+    },
+    {
+      name: "Linkedin",
+      link: "https://www.linkedin.com/",
+      image: "./assets/linkedin.png",
+    },
+    {
+      name: "Instagram",
+      link: "https://www.instagram.com/",
+      image: "./assets/instagram.png",
+    },
+    {
+      name: "E-mail",
+      link: "mailto:example.divansh@gmail.com",
+      image: "./assets/gmail.png",
+    },
+    {
+      name: "Github",
+      link: "https://github.com/",
+      image: "./assets/github.png",
+    },
+    {
+      name: "Medium",
+      link: "https://medium.com/",
+      image: "./assets/medium.png",
+    },
+    {
+      name: "GeeksforGeeks Articles",
+      link: "https://github.com/",
+      image: "./assets/geeksforgeeks.png",
+    },
+  ];
